9aea720423f4dbc7be0decae3430354dbf8cff6c
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-08-01 22:56:40 +0200

    registration form2 added

---
+++ IBM/intro.js
@@ -0,0 +1,24 @@
+var letter = "E" ;
+
+if (letter != "") {
+    if (letter == "A"){
+        //Statements
+    }
+    else if (letter == "B") {
+        //Statements 2
+    }
+    else {
+        //Nichts von oben
+    }
+    
+}
+else { 
+}
+
+//INCREMENTING THE LOOP COUNTER
+
+for(var i = minVal; i < maxVal; i++) {
+    //Statement that execute each time the for() loop
+    //cycles, as long as the condition is satisfied
+}
+
