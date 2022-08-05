9ca160dfd67b550295a1850fc17fe216fdef710a
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-08-05 22:27:10 +0200

    basic creating function added

---
+++ IBM/creating_function.js
@@ -0,0 +1,9 @@
+function add(m, n) {
+    return m + n ;
+}
+
+var x = add(1, 2) ; //returns --> 3
+
+y = add(1.23, 2.44) ; //returns --> 3.67
+
+z = add("hello", "world") // return ----> helloworld
